84e7fedd2409cfe18ffaa1fb1d1587c035a17110
f1nwe <kaban.volodymyr@gmail.com>
2016-12-05 23:25:32 +0200

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert-danger.alert'
+    assert_select 'form[action="/signup"]'
+end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name:  "Vasya_Pupkin",
+                                          email: "Pupkin.Vasya@valid.org",
+                                          password:              "1qaz2wsx",
+                                          password_confirmation: "1qaz2wsx" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
